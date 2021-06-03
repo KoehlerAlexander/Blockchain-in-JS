683144a7930287421f34d52ce93afa370cbebd69
Alexander Köhler <alexander.koehler@osapiens.com>
2021-06-03 12:25:59 +0200

    Changed Block data to be an object

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import Block from "./Block.js"
 import Blockchain from "./Blockchain.js"
 
 const myBlockchain = new Blockchain()
-const block1 = new Block(1, Date.now(), "My first Block! When will I be rich?")
-const block2 = new Block(2, Date.now(), "After the second one I must be rich. Right?")
+const block1 = new Block(1, Date.now(), { amount: 10 })
+const block2 = new Block(2, Date.now(), { amount: 15 })
 myBlockchain.addNewBlock(block1)
 myBlockchain.addNewBlock(block2)
-console.log(myBlockchain)
+console.dir(myBlockchain, { depth: null })
