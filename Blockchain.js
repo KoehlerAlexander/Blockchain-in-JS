b2de4640831df3bf0a65ab3f06664cbebd13cb66
Alexander Köhler <alexander.koehler@osapiens.com>
2021-06-04 08:57:42 +0200

    FIX blockchain now correctly checking each block for validity

--- Blockchain.js
+++ Blockchain.js
@@ -21,6 +21,24 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false
+      }
+
+      console.log(currentBlock, previousBlock)
+      if (currentBlock.previousBlock !== previousBlock.hash) {
+        return false
+      }
+
+    }
+    return true
+  }
 }
 
 export default Blockchain
--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const block1 = new Block(1, Date.now(), { amount: 10 })
 const block2 = new Block(2, Date.now(), { amount: 15 })
 myBlockchain.addNewBlock(block1)
 myBlockchain.addNewBlock(block2)
+
+myBlockchain.chain[1].data = { amount: 100 }
+myBlockchain.chain[1].hash = myBlockchain.chain[1].calculateHash()
+console.log("Blockchain is Valid: ", myBlockchain.isChainValid())
 console.dir(myBlockchain, { depth: null })
